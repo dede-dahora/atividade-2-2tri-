d43333f87f67fa0dbe300d3a31df1e95f5d440d0
dede-dahora <andresoares@colegiopaulodetarso.com.br>
2025-05-19 15:56:55 +0000

    commit

---
+++ btnAlert2/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botao = document.getElementById('meuBotao');
+    
+    botao.addEventListener('click', function() {
+        alert("Você clicou no botão!");
+    });
+});
---
+++ calculadora.js
@@ -0,0 +1,20 @@
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+console.log("Soma: 5 + 3 =", soma(5, 3));
+console.log("Subtração: 10 - 4 =", subtracao(10, 4));
+console.log("Multiplicação: 7 * 6 =", multiplicacao(7, 6));
+console.log("Divisão: 20 / 5 =", divisao(20, 5));
